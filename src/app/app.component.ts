fafc1db5be617e4149dd5261c5fca60bdfd3abb2
mstf026 <mstfylmz10@outlook.com>
2023-06-09 19:24:54 +0300

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BoschProject';
+  title: string = 'BoschProject';
+  user: string = "Mustafa Yilmaz";
+  
 }
---
+++ src/app/models/product.ts
@@ -0,0 +1,7 @@
+export interface Product{
+    id:number;
+    name:string;
+    price:number;
+    modelNumber:string;
+    type:string;
+}
