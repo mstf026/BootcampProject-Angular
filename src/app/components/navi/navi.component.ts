d84a2a442902c23019dd518d89c55dd262b5c1ae
mstf026 <mstfylmz10@outlook.com>
2023-06-13 20:09:50 +0300

    Final

--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -1,4 +1,3 @@
-import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
@@ -7,31 +6,9 @@ import { Component, Inject, OnInit } from '@angular/core';
   styleUrls: ['./navi.component.css'],
 })
 export class NaviComponent implements OnInit {
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+  constructor() {}
 
   ngOnInit(): void {}
 
-  redirectToHome(): void {
-    this.document.location.href = 'https://brave-meadow-0910b9510.3.azurestaticapps.net/';
-  }
-
-  redirectToManufacture(): void {
-    this.document.location.href = 'https://brave-meadow-0910b9510.3.azurestaticapps.net/manufactures';
-  }
-
-  redirectToAddProduct(): void {
-    this.document.location.href = 'https://brave-meadow-0910b9510.3.azurestaticapps.net/products/add';
-  }
-
-  redirectToGetOrders(): void {
-    this.document.location.href = 'http://localhost:4200/orders/';
-  }
-
-  redirectToGetDepartments(): void {
-    this.document.location.href = 'http://localhost:4200/departments/';
-  }
-
-  redirectToGetSubpieces(): void {
-    this.document.location.href = 'http://localhost:4200/subpieces/';
-  }
+  
 }
