428c31350afaab747392024d5e442541102d48bc
mstf026 <mstfylmz10@outlook.com>
2023-07-29 17:35:47 +0300

    29 temmuz 3

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,7 +10,7 @@ import { ResponseModel } from '../models/responseModel';
 })
 export class ProductService {
 
-  apiUrl:string = "http://myboschproject2612.azurewebsites.net/api/"
+  apiUrl:string = "https://myboschproject2612.azurewebsites.net/api/"
 
   constructor(private httpClient:HttpClient) { }
 
